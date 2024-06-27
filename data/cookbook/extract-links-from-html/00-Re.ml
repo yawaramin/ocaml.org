f14cc1672106e62b0f720e70f2444fa7afe0f762
Grant Smith <57376089+ggsmith842@users.noreply.github.com>
2024-06-27 10:41:20 -0600

    Update 00-Re.ml
    Simplified comments and removed `read_file` section. It is now replaced with an HTML string. I also removed the sample HTML that would render for simplicity.

--- data/cookbook/extract-links-from-html/00-Re.ml
+++ data/cookbook/extract-links-from-html/00-Re.ml
@@ -8,15 +8,29 @@ packages:
 
 (*
 
-Given an HTML document or string, we can use the `re` library to create a regular expression that finds the `href` tags containing web links.
-For example, in the sample below we would expect to find three links for this HTML document.
+`find_links` accepts an argument `html_content` of type string that contains our HTML content and returns
+the content of the `href` tags.
 
-Sample HTML: 
+You can view the pattern using [Regex101](https://regex101.com/r/2Bs442/1)
+to understand more about what is going on. 
 
+`Re.all` searches the entire `html_content` string for the `pattern`. Passing `1` to `Re.Group.get` returns the 
+substring versus the entire matching group.
+
+*)
+let find_links html_content = 
+  let pattern = Re.compile (Re.Perl.re "<a[^>]* href=\"([^\"]*)") in
+  let links = Re.all pattern html_content 
+  |> List.map (fun group -> Re.Group.get group 1) in
+  List.iter print_endline links
+
+
+(* Example usage *)
+let html_content = "
 <!DOCTYPE html>
-<html lang="en">
+<html lang=\"en\">
 <head>
-    <meta charset="UTF-8">
+    <meta charset=\"UTF-8\">
     <title>Sample HTML Page</title>
 </head>
 <body>
@@ -27,42 +41,21 @@ Sample HTML:
         <section>
             <H2>Click a link to get started!</H2>
             <ul>
-                <li><a href="https://ocaml.org/docs">The Ocaml.org Learning Page</a></li>
-                <li><a href="https://pola.rs/">Pola.rs: Modern Python Dataframes</a></li>
-                <li><a href="https://www.nonexistentwebsite.com">It used to work.com</a></li>
+                <li><a href=\"https://ocaml.org/docs\">The Ocaml.org Learning Page</a></li>
+                <li><a href=\"https://pola.rs/\">Pola.rs: Modern Python Dataframes</a></li>
+                <li><a href=\"https://www.nonexistentwebsite.com\">It used to work.com</a></li>
             </ul>
         </section>
     </main>
 </body>
-</html>
-
-`find_links` accepts an argument `html_content` of type string that contains our HTML content.
-
-Use `Re.Perl.re` to create a Perl flavored regular expression that searches for the `a href` tags. You can view the pattern using [Regex101](https://regex101.com/r/2Bs442/1)
-to understand more about what is going on. 
-
-`Re.all` searches the entire `html_content` string for the `pattern`.
-
-We then pipe the output to `List.map`, since `Re.all` returns a list of all matches and applies the `Re.group.get` function to each group
-in the list. Passing `1` we get the substring versus the entire matching group. This way we only get the URL link and not the entire HTML tag.
-
-`List.iter` iterates through the `links` list and prints the URLs.
-
-*)
-let find_links html_content = 
-  let pattern = Re.compile (Re.Perl.re "<a[^>]* href=\"([^\"]*)") in
-  let links = Re.all pattern html_content 
-  |> List.map (fun group -> Re.Group.get group 1) in
-  List.iter print_endline links
-
-
-(*
-Example usage:
-First, define the helper function's `read_file` in order to read it in HTML content at the path. Note: you may need to include `Open Stdlib` to access `In_channel`.
-*)
-let read_file file = 
-  In_channel.with_open_text file In_channel.input_all
-
+</html>"  
   
-(* Open the HTML file using `read_file` and find the links using the `find_links` function. *)
-let () = find_links (read_file "lib/webhtml/index.html")
+(*Expected output:
+
+https://ocaml.org/docs
+https://pola.rs/
+https://www.nonexistentwebsite.com
+
+*)
+let () = find_links html_content
+
