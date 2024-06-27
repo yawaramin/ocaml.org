2664b11d35dba62d3bfcb570d2c12a19b8c62c99
Christine Rose <christinerose@users.noreply.github.com>
2024-06-27 02:15:33 -0700

    formatting, grammar, verb agreement, etc.

--- data/cookbook/extract-links-from-html/00-Re.ml
+++ data/cookbook/extract-links-from-html/00-Re.ml
@@ -8,8 +8,8 @@ packages:
 
 (*
 
-Given an HTML document or string we can use the `re` library to create a regular expression that finds the href tags containing web links.
-For example in the sample below we would expect to find three links for this HTML document.
+Given an HTML document or string, we can use the `re` library to create a regular expression that finds the `href` tags containing web links.
+For example, in the sample below we would expect to find three links for this HTML document.
 
 Sample HTML: 
 
@@ -43,10 +43,10 @@ to understand more about what is going on. [Regex101](https://regex101.com/r/2Bs
 
 `Re.all` searches the entire `html_content` string for the `pattern`.
 
-We then pipe the output to `List.map` since `Re.all` returns a list of all matches and apply the `Re.group.get` function to each group
-in the list. Passing `1` we get the substring versus the entire matching group. This way we only get the url link and not the entire html tag.
+We then pipe the output to `List.map`, since `Re.all` returns a list of all matches and applies the `Re.group.get` function to each group
+in the list. Passing `1` we get the substring versus the entire matching group. This way we only get the URL link and not the entire HTML tag.
 
-`List.iter` iterates through the `links` list and prints the urls.
+`List.iter` iterates through the `links` list and prints the URLs.
 
 *)
 let find_links html_content = 
@@ -58,11 +58,11 @@ let find_links html_content =
 
 (*
 Example usage:
-First, define helper function `read_file` to read in html content at the path. Note: you may need to include `Open Stdlib` to access `In_channel`
+First, define helper function `read_file` to read in HTML content at the path. Note: you may need to include `Open Stdlib` to access `In_channel`
 *)
 let read_file file = 
   In_channel.with_open_text file In_channel.input_all
 
   
-(* Open the html file using `read_file` and find the links using the `find_links` function. *)
+(* Open the HTML file using `read_file` and find the links using the `find_links` function. *)
 let () = find_links (read_file "lib/webhtml/index.html")
