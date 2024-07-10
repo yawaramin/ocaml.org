5fbb6ff6f89157978e9dd5a2d285b7d4b0828727
Yawar Amin <yawar.amin@gmail.com>
2024-07-10 01:56:30 -0400

    Fix up lambdasoup example and add commentary

--- data/cookbook/extract-links-from-html/00-lambdasoup.ml
+++ data/cookbook/extract-links-from-html/00-lambdasoup.ml
@@ -5,32 +5,32 @@ packages:
     used_libraries:
       - lambdasoup
 discussion: |
-  - **Refernce:** The lambdasoup package provides a robust toolset for working with HTML. [github.com/lambdasoup](https://github.com/aantron/lambdasoup?tab=readme-ov-file)
+  - **Reference:** The lambdasoup package provides a robust toolset for working with HTML. [github.com/lambdasoup](https://github.com/aantron/lambdasoup)
 ---
 
-(*
-
-`find_links` accepts an argument `html_content` of type string that contains our HTML content and returns
-the content of the `href` tags.
-
-`parse` from the `Soup` library produces a document node representing the HTML string.
-
-`$$` selects nodes in the document using the selector query.
-
-*)
 open Soup
 
+(** [find_links html_content] prints out all hyperlinks found inside [href]
+attributes of [<a>] tags in the given HTML content. *)
 let find_links html_content =
+  (* [parse] parses a string containing HTML markup into a document node. *)
   let document_node = parse html_content in 
+
+  (* [$$] selects all nodes in the document matching the selector query.
+
+     [R.attribute] gets the value of an attribute from a node, raising an error
+     if the attribute doesn't exist. In this case we know there won't be any
+     error because we've already selected [<a>] tags which have the [href]
+     attribute. *)
   document_node $$ "a[href]" |> iter (fun a -> print_endline (R.attribute "href" a))
 
-
 (* Example usage *)
-let html_content = "
+
+let html_content = {|
 <!DOCTYPE html>
-<html lang=\"en\">
+<html lang="en">
 <head>
-    <meta charset=\"UTF-8\">
+    <meta charset="UTF-8">
     <title>Sample HTML Page</title>
 </head>
 <body>
@@ -39,21 +39,22 @@ let html_content = "
     </header>
     <main>
         <section>
-            <H2>Click a link to get started!</H2>
+            <h2>Click a link to get started!</h2>
             <ul>
-                <li><a href=\"https://ocaml.org/docs\">The Ocaml.org Learning Page</a></li>
-                <li><a href=\"https://pola.rs/\">Pola.rs: Modern Python Dataframes</a></li>
-                <li><a href=\"https://www.nonexistentwebsite.com\">It used to work.com</a></li>
+                <li><a href="https://ocaml.org/docs">The Ocaml.org Learning Page</a></li>
+                <li><a href="https://pola.rs/">Pola.rs: Modern Python Dataframes</a></li>
+                <li><a href="https://www.nonexistentwebsite.com">It used to work.com</a></li>
             </ul>
         </section>
     </main>
 </body>
-</html>"  
-  
-(*Expected output:
-https://ocaml.org/docs
-https://pola.rs/
-https://www.nonexistentwebsite.com
-*)
+</html>
+|}
+
+(* Expected output:
+
+   https://ocaml.org/docs
+   https://pola.rs/
+   https://www.nonexistentwebsite.com *)
 let () = find_links html_content
 
