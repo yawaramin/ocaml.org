69d66740a1a2fa2461efce1ec36acf13dfbadd9c
Yawar Amin <yawar.amin@gmail.com>
2024-07-10 18:54:10 -0400

    Clean up yaml and use Markdown in comments

--- data/cookbook/extract-links-from-html/00-lambdasoup.ml
+++ data/cookbook/extract-links-from-html/00-lambdasoup.ml
@@ -1,26 +1,25 @@
 ---
 packages:
-  - name: "lambdasoup"
-    tested_version: "1.0.0"
-    used_libraries:
-      - lambdasoup
-discussion: |
-  - **Reference:** The lambdasoup package provides a robust toolset for working with HTML. [github.com/lambdasoup](https://github.com/aantron/lambdasoup)
+- name: "lambdasoup"
+  tested_version: "1.0.0"
+  used_libraries:
+  - lambdasoup
 ---
 
 open Soup
 
-(** [find_links html_content] prints out all hyperlinks found inside [href]
-attributes of [<a>] tags in the given HTML content. *)
+(* `find_links html_content` prints out all hyperlinks found inside `href`
+   attributes of `<a>` tags in the given HTML content. *)
+
 let find_links html_content =
-  (* [parse] parses a string containing HTML markup into a document node. *)
+  (* `parse` parses a string containing HTML markup into a document node. *)
   let document_node = parse html_content in 
 
-  (* [$$] selects all nodes in the document matching the selector query.
+  (* `$$` selects all nodes in the document matching the selector query.
 
-     [R.attribute] gets the value of an attribute from a node, raising an error
+     `R.attribute` gets the value of an attribute from a node, raising an error
      if the attribute doesn't exist. In this case we know there won't be any
-     error because we've already selected [<a>] tags which have the [href]
+     error because we've already selected `<a>` tags which have the `href`
      attribute. *)
   document_node $$ "a[href]" |> iter (fun a -> print_endline (R.attribute "href" a))
 
